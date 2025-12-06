bec308643d4cb869e144aa6b0ccf637eb4428bca
Jaya <jaya.kolte21@gmail.com>
2025-12-06 14:08:13 -0600

    Day 1 practice : Scanner input program added

---
+++ day1-java-basics/src/main/java/org/example/Welcome.java
@@ -0,0 +1,17 @@
+package org.example;
+import java.util.Scanner;
+
+public class Welcome{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your name :\n");
+        String name = sc.nextLine();
+
+        System.out.println("Enter your goal for becoming a developer :");
+        String goal = sc.nextLine();
+
+        System.out.println(" Welcome  "+name+" You are on a track to become a great developer  ");
+        System.out.println("Goal : "+goal);
+    }
+}
